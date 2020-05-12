aa4fb38ba03be8ecc635e73fc9b0f0bf50778b65
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-05-12 23:06:30 +1000

    fixing some linting issue to work with prettier in vscode

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-
 const PORT = 3000;
+
+const test = () => {
+  console.log(test);
+};
+
+test();
 
 app.listen(PORT, () => {
   console.log(`App is listening at ${PORT}`);
