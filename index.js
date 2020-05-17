29f1b860a4267a7588d3a7dfcbd013c511b7de05
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-05-18 07:38:55 +1000

    normal route handler, schema creted

--- index.js
+++ index.js
@@ -1,10 +1,113 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from 'config';
+import debug from 'debug';
 
+// debug namespaces
+const startDebug = debug('app:index');
+
+// Middlewares
+const PORT = process.env.PORT || 3000;
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`App is listening at ${PORT}`);
+// Connecting to local mongodb
+mongoose
+  .connect(config.get('db'), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    startDebug('Connected to database');
+  })
+  .catch((err) => {
+    startDebug('Could not connect to database', err);
+  });
+
+// projectSchema
+
+const projectTaskSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  dueDate: Date,
 });
 
-export default app;
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  tasks: [projectTaskSchema],
+});
+
+// Creating userSchema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 55,
+  },
+  email: {
+    type: String,
+    required: true,
+    maxlength: 55,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+  isVerified: {
+    type: Boolean,
+    default: true,
+  },
+  projects: [projectSchema],
+});
+
+// Creatig user model from schema
+const User = mongoose.model('User', userSchema);
+
+const user = new User({
+  name: 'Ajeet',
+  email: 'test@gmail.com',
+  password: '12345',
+  projects: null,
+});
+
+// const createUser = async () => {
+//   try {
+//     await user2.save();
+//   } catch (er) {
+//     startDebug(JSON.stringify(er));
+//   }
+// };
+
+// Routes
+app.post('/api/users', async (req, res) => {
+  startDebug('Inside post route handler');
+
+  startDebug('type of isVerified', typeof req.body.isVerified);
+
+  const user2 = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    isVerified: req.body.isVerified,
+    projects: null,
+  });
+
+  try {
+    const result = await user2.save();
+    startDebug('post result: ', result);
+  } catch (er) {
+    startDebug(JSON.stringify(er));
+  }
+
+  res.send({ updated: true });
+});
+
+// App listening
+app.listen(PORT, () => {
+  startDebug(`App is listening at ${PORT}`);
+});
